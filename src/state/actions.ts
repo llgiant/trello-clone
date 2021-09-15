0a01cc92e8ddaf235cf75623c0412f8aea2c6405
llgiant <llgiant@gmail.com>
2021-09-15 22:21:38 +0600

    ghost column drag

--- src/state/actions.ts
+++ src/state/actions.ts
@@ -1,4 +1,7 @@
 // ADD_LIST - contains the list title.
+
+import { DragItem } from "../DragItem"
+
 //ADD_TASK - text is the task text, and listId is the reference to the list it belongs to.
 export type Action =
     | {
@@ -8,33 +11,60 @@ export type Action =
     | {
     type: "ADD_TASK"
     payload: { text: string; listId: string }
+
 }
-//It would work same way, I just prefer using types.
-// The technique we are using here is called discriminated union
-// interface AddListAction {
-//     type: "ADD_LIST"
-//     payload: string
-// }
-// interface AddTaskAction {
-//     type: "ADD_LIST"
-//     payload: { text: string; listId: string }
-// }
+    | {
+    type: "MOVE_LIST";
+    payload: { draggedId: string; hoverId: string }
+
+}
+    | {
+    type: "SET_DRAGGED_ITEM"
+    payload: DragItem | null
+}
+
+//It will hold the DragItem that we defined earlier. We need to be able to set it to null if
+// we are not dragging anything. We are not using the undefined here because it would
+// mean that the field could be omitted. In our case it’s not true, it can just be empty
+// sometimes.
+// Define the action creator
+
+//MOVIE_LIST action has draggedId and hoverId in its
+// payload. When we start dragging the column, we remember its id and pass it as
+// draggedId. When we hover over other columns we take their ids and use them as a
+// hoverId
 
 //action creators.
 
-export const addTask = (
-    text: string,
-    listId: string,
+export const setDraggedItem = (
+    draggedItem: DragItem | null,
 ): Action => ({
-    type: "ADD_TASK",
-    payload: {
-        text,
-        listId
-    }
+    type: "SET_DRAGGED_ITEM",
+    payload: draggedItem
 })
+
+
+export const moveList = (draggedId: string, hoverId: string,): Action =>
+    ({
+        type: "MOVE_LIST",
+        payload: {
+            draggedId,
+            hoverId,
+        }
+    })
+
+export const addTask = (text: string, listId: string,): Action =>
+    ({
+        type: "ADD_TASK",
+        payload: {
+            text,
+            listId
+        }
+    })
 export const addList = (
     text: string,
 ): Action => ({
     type: "ADD_LIST",
     payload: text
 })
+
