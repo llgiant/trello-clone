8ffc0fff892a5d7253d39f51d4892ea0a9861689
Bogdan Kuznetsov <llgiant@gmail.com>
2021-09-15 21:30:30 +0600

    utils/arrayUtils.ts

--- src/utils/arrayUtils.ts
+++ src/utils/arrayUtils.ts
@@ -1,5 +1,3 @@
-
-
 type Item = {
     id: string
 }
@@ -10,6 +8,14 @@ export const findItemIndexById = <TItem extends Item>(
 ) => {
     return items.findIndex((item: TItem) => item.id === id)
 }
+//utility function that will help us to move the items inside the array
+
+export const moveItem = <TItem>(array: TItem[], from: number, to: number) => {
+    const item = array[from]
+    return insertItemAtIndex(removeItemAtIndex(array, from), item, to)
+}
+//We want to be able to work with arrays with any kind of items in them, so we use a
+// type variable TItem.
 
 
 
